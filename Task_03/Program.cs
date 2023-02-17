160c3eb0df2b8077879c2da568f0fdc85862ea31
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-17 19:11:34 +0300

    Task_03 added

---
+++ Task_03/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да   7 -> да      1 -> нет
+
+int Prompt(string msg)
+{
+    Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Weekend(int DayOfWeek)
+{
+    if(DayOfWeek > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool DayNum(int num)
+{
+    if(num > 0 && num < 8 )
+    {
+        return true;
+    }
+    Console.WriteLine("Такого дня недели нет");
+    return false;
+}
+
+int DayOfWeek = Prompt("Введите номер дня недели -> ");
+if(DayNum(DayOfWeek))
+{
+    if(Weekend(DayOfWeek))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
