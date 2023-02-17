6bb901b5bbb4e5a7e4d08ab499970078aa1944f8
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-17 18:53:00 +0300

    Task_02 added

---
+++ Task_02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5  78 -> третьей цифры нет  32679 -> 6
+
+int Prompt(string msg)
+{
+    Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ThirdDigit(int number)
+{
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    return (number % 10);
+}
+
+bool ValidateNum(int number)
+{
+    if(number < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+        return false;
+    }
+        return true;
+}
+
+int number = Prompt("Введите число -> ");
+if(ValidateNum(number))
+{
+    Console.WriteLine(ThirdDigit(number));
+}
